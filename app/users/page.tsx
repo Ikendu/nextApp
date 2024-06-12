0a97874d20c33fc1a448930cef8a86c2e436fdc8
Aniede Chibundu <ndubest56@gmail.com>
2024-06-12 14:04:16 +0100

    fetching user data

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,3 +1,19 @@
-export default function UsersPage() {
-  return <div>All users page</div>;
+import getAllUsers from "@/lib/getAllUsers";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "User page",
+  description: "show all users availabe",
+};
+
+export default async function UsersPage() {
+  const usersData: Promise<> = await getAllUsers();
+
+  console.log(usersData);
+
+  return (
+    <div>
+      <h1>All users page</h1>
+    </div>
+  );
 }
---
+++ lib/getAllUsers.ts
@@ -0,0 +1,8 @@
+export default async function getAllUsers() {
+  const users = await fetch("https://jsonplaceholder.typicode.com/users");
+  if (users.ok) {
+    return users.json();
+  } else {
+    throw new Error("No user found");
+  }
+}
