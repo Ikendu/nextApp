7ce815b27fbc5d743812a72705570b01e8709e05
Aniede Chibundu <ndubest56@gmail.com>
2024-06-16 23:11:36 +0100

    search component page

--- app/users/[userId]/components/Navbar.tsx
+++ app/users/[userId]/components/Navbar.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 function Navbar() {
   return (
-    <nav className=" bg-slate-600 p-4">
-      <h1>
+    <nav className=" bg-slate-600 p-4 flex justify-between flex-col md:flex-row sticky top-0 drop-shadow-xl">
+      <h1 className="text-3xl font-bold text-white grid place-content-center mb-2 md:mb-0">
         <Link href={"/"}>WikiLife</Link>
       </h1>
     </nav>
