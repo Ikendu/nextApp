49e6ff623398275471eaef4b0bdaa98731d9b06d
Aniede Chibundu <ndubest56@gmail.com>
2024-06-16 23:39:42 +0100

    search component page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "./users/[userId]/components/Navbar";
+import Navbar from "./users/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
---
+++ app/users/components/Search.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { FormEvent, useState } from "react";
+
+export default function Search() {
+  const [search, setSearch] = useState("");
+  const router = useRouter();
+
+  const handleSearchTerms = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setSearch("");
+    router.push(`/${search}/`);
+  };
+  return (
+    <form
+      onSubmit={handleSearchTerms}
+      className="w-50 flex justify-center md:justify-between"
+    >
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        placeholder="Search"
+        className="p-2 w-80 text-xl bg-white rounded-xl"
+      />
+      <button className="p-2 font-bold text-xl ml-2 rounded-xl bg-slate-300">
+        🧬
+      </button>
+    </form>
+  );
+}
