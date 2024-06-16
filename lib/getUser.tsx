f9d775b66508f202e6d47eb5499124c66c2550b4
Aniede Chibundu <ndubest56@gmail.com>
2024-06-16 18:22:22 +0100

    revalidate fetched data

--- lib/getUser.tsx
+++ lib/getUser.tsx
@@ -1,6 +1,9 @@
 export default async function getUser(userId: string) {
   const user = await fetch(
-    `https://jsonplaceholder.typicode.com/users/${userId}`
+    `https://jsonplaceholder.typicode.com/users/${userId}`,
+    // { cache:"no-cache" } //to avoid the default behavoir of storing and reusing of fetched data
+    // we also have 'forced-cashe: this is the default
+    { next: { revalidate: 60 } } // this update the fetched data every 60 seconds. this can be applied to either static site or server side
   );
 
   if (!user.ok) throw new Error("User not found");
