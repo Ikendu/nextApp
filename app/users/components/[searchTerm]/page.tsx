2826b0ff9de8708f3b566b423303dd537c638b74
Aniede Chibundu <ndubest56@gmail.com>
2024-06-17 05:56:36 +0100

    getting search result

--- app/users/components/[searchTerm]/page.tsx
+++ app/users/components/[searchTerm]/page.tsx
@@ -7,9 +7,20 @@ type Props = {
     searchTerm: string;
   };
 };
-export default async function page({ params: { searchTerm } }: Props) {
+export default async function SearchResults({ params: { searchTerm } }: Props) {
   const wikiData: Promise<SearchResult> = getWikiResults(searchTerm);
 
   const data = await wikiData;
-  return <div>page</div>;
+  const results: Result[] | undefined = data?.query?.pages;
+
+  const content = (
+    <main className="bg-slate-200 ma-auto max-w-lg py-1 min-h-screen">
+      {results ? (
+        Object.values(results).map((result) => <p>{JSON.stringify(result)}</p>)
+      ) : (
+        <h3>{`${searchTerm} NOT FOUND`}</h3>
+      )}
+    </main>
+  );
+  return content;
 }
