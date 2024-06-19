8e699b10dda7f0bfba28c6687d8db1ef7750e4e2
Aniede Chibundu <ndubest56@gmail.com>
2024-06-19 07:26:46 +0100

    getting a post

--- app/aboutlife/posts/[postId]/page.tsx
+++ app/aboutlife/posts/[postId]/page.tsx
@@ -1,3 +1,10 @@
-export default function page({ params }: post) {
+import getSortedPostsData from "@/lib/post";
+import { notFound } from "next/navigation";
+
+export default function Post({ params }: { params: { postId: string } }) {
+  const { postId } = params;
+  const posts = getSortedPostsData();
+
+  if (!posts.find((post) => post.id === postId)) return notFound();
   return <div>page for post</div>;
 }
