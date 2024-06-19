d7f88d523e187097f9f6703aa315c0d240d540c4
Aniede Chibundu <ndubest56@gmail.com>
2024-06-19 08:20:14 +0100

    getting a single post

--- lib/post.tsx
+++ lib/post.tsx
@@ -1,6 +1,8 @@
 import fs from "fs";
 import matter from "gray-matter";
 import path from "path";
+import { remark } from "remark";
+import html from "remark-html";
 
 //cwd: Current Worink Directory
 const postDirectory = path.join(process.cwd(), "blogposts");
@@ -27,4 +29,24 @@ export default function getSortedPostsData() {
     return blogPost;
   });
   return allPostData.sort((a, b) => (a.date < b.date ? 1 : -1));
+}
+
+export async function getPostData(id: string) {
+  // Read markdown files as string
+  const fullPath = path.join(postDirectory, `${id}.md`);
+  const fileContents = fs.readFileSync(fullPath, "utf8");
+
+  // gray-matter is used to parse the post metaData section
+  const matterResult = matter(fileContents);
+
+  const processContent = await remark().use(html).process(matterResult.content);
+
+  const contentHtml = processContent.toString();
+  const blogPostWithHtml: BlogPost & { contentHtml: string } = {
+    id,
+    title: matterResult.data.title,
+    date: matterResult.data.date,
+    contentHtml,
+  };
+  return blogPostWithHtml;
 }
