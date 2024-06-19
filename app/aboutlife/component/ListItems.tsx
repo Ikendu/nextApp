d7a5aed67bdd1c130a7a8745aaf525cb7658d402
Aniede Chibundu <ndubest56@gmail.com>
2024-06-19 06:29:54 +0100

    rendering blog posts

--- app/aboutlife/component/ListItems.tsx
+++ app/aboutlife/component/ListItems.tsx
@@ -1,8 +1,23 @@
+import getFormattedDate from "@/lib/getFormattedDate";
+import Link from "next/link";
+
 type Props = {
-  posts: BlogPost;
+  post: BlogPost;
 };
-const ListItems = ({ posts }: Props) => {
-    const {id, title, date} = posts
-  return <div>ListItems</div>;
+const ListItems = ({ post }: Props) => {
+  const { id, title, date } = post;
+  //   const formattedDate = getFormattedDate(date);
+  return (
+    <li className="mt-4 text-2xl dark:text-white/70">
+      <Link
+        href={`/posts/${id}`}
+        className="underline hover:text-black/70 dark:hover:text-white"
+      >
+        {title}
+      </Link>
+      <br />
+      <p className="text-sm mt-1">{date}</p>
+    </li>
+  );
 };
 export default ListItems;
