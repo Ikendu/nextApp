cbe34bd2a9d3df34d3917e92c7c293177751a1d6
Aniede Chibundu <ndubest56@gmail.com>
2024-06-22 06:18:32 +0100

    using git fetch api

--- lib/post.tsx
+++ lib/post.tsx
@@ -1,3 +1,7 @@
+type Filetree = {
+  tree: [{ path: string }];
+};
+
 export async function getPostMeta(): Promise<Meta[] | undefined> {
   const res = await fetch(
     `https://api.github.com/repos/Ikendu/blogpost/git/trees/main?recursive=1`,
@@ -9,4 +13,10 @@ export async function getPostMeta(): Promise<Meta[] | undefined> {
       },
     }
   );
+  if (!res.ok) return undefined;
+  const repoFileTree: Filetree = await res.json();
+  console.log(`REPO FILE TREE`, repoFileTree);
+
+  const filesArray = repoFileTree.tree.map((obj) => obj.path);
+  filesArray.filter((path) => path.endsWith(`.mdx`));
 }
