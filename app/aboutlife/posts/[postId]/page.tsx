6968e2c5401bdd6862c8537c08dbec87a5ccde99
Aniede Chibundu <ndubest56@gmail.com>
2024-06-23 17:13:39 +0100

    getting a post

--- app/aboutlife/posts/[postId]/page.tsx
+++ app/aboutlife/posts/[postId]/page.tsx
@@ -1,36 +1,49 @@
-import getSortedPostsData, { getPostData } from "@/lib/post";
+// import getSortedPostsData, { getPostData } from "@/lib/post";
+import { getPostByName, getPostMeta } from "@/lib/post";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 
 //to make our post server-side rendered (will not cache anything)
 export const revalidate = 0;
+type Props = {
+  params: { postId: string };
+};
 
 // to use static site generation and improve our website rendering
 // rebuild this project!
-export function generateStaticParams() {
-  const posts = getSortedPostsData();
-  return posts.map((post) => ({ postId: post.id }));
-}
+// export function generateStaticParams() {
+//   const posts = await getPostMeta();
+//   if (!posts) return []; // returning empty array is better than undefined so we dont have problems during mapping an undefined
 
-export function generateMetadata({ params }: { params: { postId: string } }) {
-  const { postId } = params;
-  const posts = getSortedPostsData();
+//   return posts.map((post) => ({ postId: post.id }));
+// }
 
-  const post = posts.find((post) => post.id === postId);
+// getting the title page for each post
+export async function generateMetadata({ params: { postId } }: Props) {
+  // pass params and destructure it
+  const post = await getPostByName(`${postId}.mdx`); //deduped
 
-  if (!post) return { title: `Post not found` };
+  if (!post) return { title: `Post Not Found` }; // returning empty array is better than undefined so we dont have problems during mapping an undefined
+
   return {
-    title: post.title,
+    title: post.meta.title,
   };
 }
 
-export default async function Post({ params }: { params: { postId: string } }) {
+export default async function Post({ params }: Props) {
   const { postId } = params;
-  const posts = getSortedPostsData();
+  const post = await getPostByName(`${postId}.mdx`); //deduped
 
-  if (!posts.find((post) => post.id === postId)) return notFound();
+  if (!post) notFound();
 
-  const { id, title, date, contentHtml } = await getPostData(postId);
+  const { meta, content } = post;
+  console.log(meta, content);
+
+  const tags = meta.tags.map((tag, idx) => (
+    <Link key={idx} href={`/tags/${tag}`}>
+      {tag}
+    </Link>
+  ));
   return (
     <main className="px-6 prose prose-xl prose-slate mx-auto dark:prose-invert">
       <h1 className="text-3xl mt-4 mb-0">{title}</h1>
